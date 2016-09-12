86602e76f980c5a6a5c7fd783e768c4f60565643
TechStack <techStackLp@gmail.com>
2016-09-12 17:04:51 -0400

    update Te for diff in TE update method vs 1.8 way.

--- src/main/java/com/projectreddog/ecoshop/tileentities/TileEntityBuyShop.java
+++ src/main/java/com/projectreddog/ecoshop/tileentities/TileEntityBuyShop.java
@@ -4,7 +4,6 @@ import com.projectreddog.ecoshop.item.ItemEcoShopUpgrade;
 import com.projectreddog.ecoshop.reference.Reference;
 import com.projectreddog.ecoshop.utility.LogHelper;
 
-import net.minecraft.client.renderer.texture.ITickable;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.ISidedInventory;
 import net.minecraft.item.ItemStack;
@@ -12,50 +11,58 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
 
-public class TileEntityBuyShop extends TileEntity implements ITickable, ISidedInventory {
+public class TileEntityBuyShop extends TileEntity implements ISidedInventory {
 	protected ItemStack[] inventory;
 	// upper left (accept this owner area)
-	//0-8
+	// 0-8
 	// Middle top owner
-	//9-17
+	// 9-17
 	// upgrade slots
-	//18-19
-	// customer slots left 
-    //20-28
+	// 18-19
+	// customer slots left
+	// 20-28
 	// customer slots right
-    //29-43
+	// 29-43
 	// Owner only STOCK area
-	//44-97
+	// 44-97
 
 	@Override
-	public void tick() {
+	public void updateEntity() {
 		// TODO Auto-generated method stub
-		LogHelper.debug( "Range Check Returned :" +isRangeInSecondRange( 0, 8, 9, 17));
+		LogHelper.info("Range Check Returned :" + isRangeInSecondRange(0, 8, 20, 28));
 	}
-	public boolean isRangeInSecondRange( int start, int end, int secondStart, int secondEnd){
+
+	public boolean isRangeInSecondRange(int start, int end, int secondStart, int secondEnd) {
 		// is everything in the first range contained in the second range?
 		ItemStack toCheck;
 		ItemStack needed;
-		for (int neededSlot = start; neededSlot <=end;neededSlot++){
+		for (int neededSlot = start; neededSlot <= end; neededSlot++) {
 
-			boolean wasMatchFound =false;
-			for (int checkslot = secondStart; checkslot <=secondEnd;checkslot++){
+			boolean wasMatchFound = false;
+			for (int checkslot = secondStart; checkslot <= secondEnd; checkslot++) {
 
 				toCheck = inventory[checkslot];
-				needed=inventory[checkslot];
-				
-				if ( toCheck.getItem() ==needed.getItem() && toCheck.getItemDamage() == needed.getItemDamage()){
+				needed = inventory[neededSlot];
+				if (needed == null) {
 					wasMatchFound = true;
 					break;
 				}
-				
-			}		
-			if (wasMatchFound ==false){
+				if (toCheck == null) {
+					continue;
+				}
+				if (toCheck.getItem() == needed.getItem() && toCheck.getItemDamage() == needed.getItemDamage() && needed.stackSize <= toCheck.stackSize) {
+					wasMatchFound = true;
+					break;
+				}
+
+			}
+			if (wasMatchFound == false) {
 				return false;
 			}
 		}
 		return true;
 	}
+
 	public TileEntityBuyShop() {
 		inventory = new ItemStack[98];
 
@@ -144,10 +151,10 @@ public class TileEntityBuyShop extends TileEntity implements ITickable, ISidedIn
 	@Override
 	public boolean isItemValidForSlot(int slot, ItemStack itemStack) {
 		// TODO Auto-generated method stub
-		if (slot== 18 || slot ==19){
-			if (itemStack.getItem() instanceof ItemEcoShopUpgrade){
+		if (slot == 18 || slot == 19) {
+			if (itemStack.getItem() instanceof ItemEcoShopUpgrade) {
 				return true;
-			}else{
+			} else {
 				return false;
 			}
 		}
@@ -172,7 +179,6 @@ public class TileEntityBuyShop extends TileEntity implements ITickable, ISidedIn
 		return false;
 	}
 
-	
 	@Override
 	public void readFromNBT(NBTTagCompound compound) {
 
@@ -189,7 +195,7 @@ public class TileEntityBuyShop extends TileEntity implements ITickable, ISidedIn
 			}
 		}
 	}
-	
+
 	@Override
 	public void writeToNBT(NBTTagCompound compound) {
 		super.writeToNBT(compound);
