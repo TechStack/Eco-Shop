64ef7ba687fe59d66b2cdac781f9b43af0430c0d
TechStack <techStackLp@gmail.com>
2016-09-19 17:39:23 -0400

    build test

--- src/main/java/com/projectreddog/ecoshop/EcoShop.java
+++ src/main/java/com/projectreddog/ecoshop/EcoShop.java
@@ -10,9 +10,8 @@ import com.projectreddog.ecoshop.reference.Reference;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;/build test @Mod(modid=EcoShop.MODID,version=EcoShop.VERSION,name=EcoShop.NAME)
 
-@Mod(modid = EcoShop.MODID, version = EcoShop.VERSION, name = EcoShop.NAME)
 public class EcoShop {
 	public static final String MODID = Reference.MODID;
 	public static final String VERSION = "1.0";
